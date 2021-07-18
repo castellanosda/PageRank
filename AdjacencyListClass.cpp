041ddb0b9b825bb94a11aba2b39dacdcb4a161cf
Daniel Castellanos <alejandro@Daniels-MacBook-Air.local>
2021-07-18 16:33:56 -0400

    changed outdegree calculations

--- AdjacencyListClass.cpp
+++ AdjacencyListClass.cpp
@@ -15,9 +15,9 @@ template<typename T>
 int AdjacencyList<T>::vertexCount() {return vCount + 1;}
 
 template<typename T>
-int AdjacencyList<T>::edgeCount(T key)
+int AdjacencyList<T>::outDegrees(T key)
 {
-    return edgeCounter.at(indexMap[key]);
+    return outdegrees.at(indexMap[key]);
 }
 
 template<typename T>
@@ -33,24 +33,24 @@ void AdjacencyList<T>::insert(T from, T to, int weight)
     //indexMap will be a powerful tool that will attach a string
     //to an index for easier iteration
     //i.e. A  0              1
-    //     B  1 <- indexMap  1 <-edgeCount vector will index
-    //     C  2 edgeCount -> 1   appropriately as well
+    //     B  1 <- indexMap  0 <-outdegrees vector will index
+    //     C  2 edgeCount -> 0   appropriately as well
     //     |  |              |
     if(indexMap.find(from) == indexMap.end())
     {
         indexMap[from] = vCount++;
-        edgeCounter.push_back(1);
+        outdegrees.push_back(0);
     }
         
     if(indexMap.find(to) == indexMap.end())
     {
         indexMap[to] = vCount++;
-        edgeCounter.push_back(1);
+        outdegrees.push_back(0);
     }
 
     //Adds edge between from and to and increments edge count
     adjacencyList[from].push_back(make_pair(to, weight));
-    edgeCounter.at(indexMap[from])++;
+    outdegrees.at(indexMap[from])++;
 
     //checks to see if to is in adjacencyList and adds if not found
     if(adjacencyList.find(to) == adjacencyList.end())
@@ -66,8 +66,11 @@ std::vector<float> AdjacencyList<T>::pageRank(int pIterations)
 {
     std::vector<float> r;
 
-    for(int i = 0; i < vCount + 1; i++)
-        r.push_back(1/(vCount + 1));
+    for(int i = 0; i < vCount; i++)
+    {
+        r.push_back(1.0/((float)vCount));
+        std::cout << r.at(i) << std::endl;
+    }
     
     //powerIteration
     while(pIterations > 1)
